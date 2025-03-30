2358b5d33a4cf2d7491686a1ad1b8163e1c44464
Mikolaj Drozdz <mikodrozdz13@gmail.com>
2025-03-31 01:09:47 +0200

    10. Dodano getter i setter dla total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -10,6 +10,14 @@ public class DefaultCountingOutRhymer {
 
     private int total = EMPTY_VALUE;
 
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
